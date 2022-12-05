feb51d3d6ade8335ae8ca59b38b085cfd256219b
MelvolaMonique <monique.lima10@etec.gov.br>
2022-12-05 08:59:24 -0300

    terminando o video 3 do ronan

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -10,14 +10,41 @@ import{FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class CadastroPage implements OnInit {
 
   formCadastro: FormGroup;
-
-
+  mensagens = {
+    nome: [
+      { tipo: 'required', mensagem: 'O campo Nome é obrigatório.' },
+      { tipo: 'minlength', mensagem: 'O nome deve ter pelo menos 3 caracteres.' },
+    ],
+    cpf: [
+      { tipo: 'required', mensagem: 'O campo CPF é obrigatório.' },
+      { tipo: 'invalido', mensagem: 'CPF Inválido.' },
+    ],
+    email: [
+      { tipo: 'required', mensagem: 'O campo E-mail é obrigatório.' },
+      { tipo: 'email', mensagem: 'E-mail Inválido.' },
+    ],
+    senha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+    ],
+    confirmaSenha: [
+      { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+      { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+      { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+      { tipo: 'comparacao', mensagem: 'Deve ser igual a Senha.' },
+    ],
+  };
 
   constructor( private formBuilder: FormBuilder) {
 
     this.formCadastro = this.formBuilder.group({
       nome: ['', Validators.compose([Validators.required,Validators.minLength(3)])],
-      email: ['',Validators.compose([Validators.required, Validators.email])]
+      cpf: ['',Validators.compose([Validators.required])],
+      email: ['',Validators.compose([Validators.required, Validators.email])],
+      senha:['',Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])],
+      confirmasenha: ['',Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(8)])]
+
     });
    }
 
