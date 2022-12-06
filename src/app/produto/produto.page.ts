1e3c41b1e27be49a8f783637974a9c496ac458ac
Monique Evelyn de Lima <monique.lima10@etec.gov.br>
2022-12-06 12:12:45 -0300

    projeto pronto só enfeitar

--- src/app/produto/produto.page.ts
+++ src/app/produto/produto.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Produto } from '../models/produto';
+import { StorageService } from '../services/storage.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,9 +13,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ProdutoPage implements OnInit {
 
 formProduto: FormGroup;
+produto: Produto = new Produto();
+
+mensagens = {
+  nome: [
+    {tipo: 'required', mensagem : 'O campo nome é obrigatório!'}
+  ],
+  descricao: [
+    {tipo: 'required', mensagem: 'A descrição é necessária'}
+  ],
+  validade: [{
+    tipo: 'required', mensagem: 'A validade é necessária'
+  }],
+  preco: [{
+    tipo: 'required', mensagem: 'O preço é necessário'
+  }]
+};
 
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private storageService: StorageService, private route: Router) {
     this.formProduto = this.formBuilder.group({
       nome: ['',Validators.compose([Validators.required, Validators.minLength(3)])],
       descricao: ['', Validators.compose([Validators.required,])],
@@ -21,6 +40,19 @@ formProduto: FormGroup;
     });
   }
   ngOnInit() {
+
   }
 
+   async salvarProduto(){
+  if(this.formProduto.valid){
+    this.produto.nome = this.formProduto.value.nome;
+    this.produto.descricao = this.formProduto.value.descricao;
+    this.produto.validade = this.formProduto.value.validade;
+    this.produto.preco = this.formProduto.value.preco;
+    await this.storageService.set(this.produto.preco, this.produto);
+    this.route.navigateByUrl('/tabs/tab1');
+  } else{
+    alert('Formulário Inválido!');
+  }
+  }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
+
 import { StorageService } from './../services/storage.service';
 import { Component } from '@angular/core';
 import { Usuario } from '../models/usuario';
+import { Produto } from '../models/produto';
 
 @Component({
   selector: 'app-tab1',
@@ -10,18 +12,30 @@ import { Usuario } from '../models/usuario';
 export class Tab1Page {
 
   listaUsuarios: Usuario[] = [];
+  listaProdutos: Produto[] = [];
 
   constructor(private storageService: StorageService) {}
 
+  async buscarProdutos() {
+    this.listaProdutos = await this.storageService.getAll();
+
+  }
 async buscarUsuarios(){
   this.listaUsuarios = await this.storageService.getAll();
 }
 
 ionViewDidEnter(){
   this.buscarUsuarios();
+  this.buscarProdutos();
 }
  async excluirCadastro(email: string){
  await this.storageService.remove(email);
  this.buscarUsuarios();
 }
+async excluirProdutos(preco: string){
+  await this.storageService.remove(preco);
+  this.buscarProdutos();
+ }
 }
+
+
